1be8f67afd6db2826a92ce42f535b66652e96c8d
Ali Büyükkaytan <a.buyukkaytan@gmail.com>
2022-06-24 23:10:39 +0300

    hashmap added in hackerrank study

---
+++ src/hackerrank/MapNamePhoneNumber.java
@@ -0,0 +1,56 @@
+package hackerrank;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * Sample Input
+ * 3
+ * uncle sam
+ * 99912222
+ * tom
+ * 11122222
+ * harry
+ * 12299933
+ * uncle sam
+ * uncle tom
+ * harry
+ *
+ * Sample Output
+ * uncle sam=99912222
+ * Not found
+ * harry=12299933
+ */
+
+public class MapNamePhoneNumber {
+    public static void main(String []argh)
+    {
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        in.nextLine();
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+
+        for(int i=0;i<n;i++)
+        {
+            String name=in.nextLine();
+            int phone=in.nextInt();
+            in.nextLine();
+            map.put(name, phone);
+        }
+        while(in.hasNext())
+        {
+            String s=in.nextLine();
+            if(map.containsKey(s)){
+                System.out.println(s + "="+map.get(s));
+            } else {
+                System.out.println("Not found");
+                if(s.equals("q")){
+                    break;
+                }
+            }
+
+        }
+    }
+}
