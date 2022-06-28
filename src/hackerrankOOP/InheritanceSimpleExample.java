ab4aec889fa01c83a2bcd572f4122ad0f6de38ab
Ali Büyükkaytan <a.buyukkaytan@gmail.com>
2022-06-28 23:54:06 +0300

    Hackerrank OOP study (inheritance)

---
+++ src/hackerrankOOP/InheritanceSimpleExample.java
@@ -0,0 +1,27 @@
+package hackerrankOOP;
+
+class Animal{
+    void walk(){
+        System.out.println("I am walking");
+    }
+}
+
+class Bird extends Animal{
+    void fly(){
+        System.out.println("I am flying");
+    }
+    void sing(){
+        System.out.println("I am singing");
+    }
+}
+
+public class InheritanceSimpleExample {
+    public static void main(String args[]){
+
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+
+    }
+}
